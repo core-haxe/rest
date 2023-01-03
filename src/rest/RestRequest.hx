1bb0bac84d9e5f50bc156e88bd12c77a8ed54e5e
Ian Harrigan <ianharrigan@hotmail.com>
2023-01-03 19:43:06 +0100

    addHeader function / clone function

--- src/rest/RestRequest.hx
+++ src/rest/RestRequest.hx
@@ -10,7 +10,34 @@ class RestRequest {
     public var queryParams:Map<String, Any>;
     public var headers:Map<String, Any>;
     public var body:Any;
+    public var useAlternateConfig:Bool = false;
 
     public function new() {
+    }
+
+    public function addHeader(name:String, value:Any) {
+        if (headers == null) {
+            headers = [];
+        }
+        headers.set(name, value);
+    }
+
+    public function clone():RestRequest {
+        var c = new RestRequest();
+        c.verb = this.verb;
+        c.path = this.path;
+        if (this.urlParams != null) {
+            c.urlParams = this.urlParams.copy();
+        }
+        if (this.queryParams != null) {
+            c.queryParams = this.queryParams.copy();
+        }
+        if (this.headers != null) {
+            c.headers = this.headers.copy();
+        }
+        c.body = this.body;
+        c.useAlternateConfig = this.useAlternateConfig;
+
+        return c;
     }
 }
